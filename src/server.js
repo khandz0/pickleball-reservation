fb9fe02b64e5294e8c3a7fe5d08010bb5d6d4501
khandz0 <kieuth24@gmail.com>
2025-03-19 18:39:59 -0700

    Update server to add code

--- src/server.js
+++ src/server.js
@@ -18,3 +18,6 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+const courtRoutes = require('./routes/courts');
+app.use('/courts', courtRoutes);
+
