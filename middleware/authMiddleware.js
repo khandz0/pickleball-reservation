0d6c393441aaf4c49b22ff80a85d33a41f685302
tkk2606 <kieuth24@gmail.com>
2025-03-22 14:42:38 -0700

    Update auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateUser = (req, res, next) => {
-    const token = req.header('Authorization');
+    const authHeader = req.header('Authorization');
 
-    if (!token) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({ message: 'Access denied' });
     }
+
+    const token = authHeader.split(' ')[1]; // Extract just the token
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
+        return res.status(401).json({ message: 'Invalid token' });
     }
 };
 
 module.exports = authenticateUser;
+
