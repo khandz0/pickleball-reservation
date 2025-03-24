5827f4c7829bc86ac7c8f159197a0f59136fcf14
tkk2606 <kieuth24@gmail.com>
2025-03-23 21:58:26 -0700

    Update server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
@@ -11,23 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const authRoutes = require('../routes/auth');
-const courtRoutes = require('../routes/courts');
-const reservationRoutes = require('../routes/reservations');
+const authRoutes = require("../routes/auth");
+const courtRoutes = require("../routes/courts");
+const reservationRoutes = require("../routes/reservations");
+const adminRoutes = require("../routes/adminRoutes");
 
-
-app.use('/auth', authRoutes);
-app.use('/courts', courtRoutes);
-app.use('/reservations', reservationRoutes);
-
+app.use("/auth", authRoutes);
+app.use("/courts", courtRoutes);
+app.use("/reservations", reservationRoutes);
+app.use("/admin", adminRoutes);
 
 // Base route
-app.get('/', (req, res) => {
-    res.send('Pickleball Court Reservation API is running...');
+app.get("/", (req, res) => {
+  res.send("Pickleball Court Reservation API is running...");
 });
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
