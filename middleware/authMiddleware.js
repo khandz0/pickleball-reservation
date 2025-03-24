4a200c38cecdbe56aa812298fcefaddadbac6ea9
tkk2606 <kieuth24@gmail.com>
2025-03-23 20:59:57 -0700

    admin login fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-    const authHeader = req.header('Authorization');
+  const authHeader = req.header("Authorization");
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(403).json({ message: 'Access denied' });
-    }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({ message: "Access denied" });
+  }
 
-    const token = authHeader.split(' ')[1]; // Extract just the token
+  const token = authHeader.split(" ")[1]; // Extract just the token
+  console.log("Incoming token:", token);
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 module.exports = authenticateUser;
-
